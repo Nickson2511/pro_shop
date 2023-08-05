0ff2f6c25c76f5cec773ee5aee2008db5a6c2824
Nickson2511 <okwembanickson8@gmail.com>
2023-08-05 11:19:58 +0300

    building a shop online store

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <header>
+    <a href="/">Shop_Nick</a>
+    </header>
+    <main>
+    List Products
+    </main>
+    </div>
+  );
+}
+
+export default App;
